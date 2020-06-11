1bda77f972745d90aeb585acef3c94fbcd260eb9
LMorel77 <lmorel77@aol.com>
2020-06-11 17:37:13 -0400

    Added database config for Heroku

--- server.js
+++ server.js
@@ -18,16 +18,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // PostgreSQL Database
-const db = knex({
-    client: 'pg',
-    connection: {
-        host: 'localhost', // '127.0.0.1'
-        user: 'ztm_admin',
-        password: 'ztmr0ck5!',
-        database: 'face_recognition'
-    }
-});
-
+if (process.env.DATABASE_URL) {
+    const db = knex({
+        client: 'pg',
+        connection: {
+            connectionString: process.env.DATABASE_URL,
+            ssl: true
+        }
+    });
+} else {
+    const db = knex({
+        client: 'pg',
+        connection: {
+            host: 'localhost', // 127.0.0.1
+            user: 'ztm_admin',
+            password: 'ztmr0ck5!',
+            database: 'face_recognition'
+        }
+    });
+}
 /*
 ==================================
         Back-End API
