9d03c8122f82b0716581a3ace0c1b75ef47986e1
LMorel77 <lmorel77@aol.com>
2020-06-11 20:58:25 -0400

    Removed client-build from gitignore

---
+++ client/build/precache-manifest.fff068b1325783897fd75650b2b6d7fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e26011105f31b1ce93a44f80840d48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "393d632f05be30ec2b65",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1e6097ec8991824151c5",
+    "url": "/static/css/main.5a606c37.chunk.css"
+  },
+  {
+    "revision": "393d632f05be30ec2b65",
+    "url": "/static/js/2.f71bd396.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.f71bd396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6097ec8991824151c5",
+    "url": "/static/js/main.c71d637e.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
